78627f87c9cb57fd9934a3f4405b385def013efb
Andreeez <32762098+Andreeez@users.noreply.github.com>
2017-11-16 09:18:25 +0100

    Structure and Login update

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 console.log("hej");
+
+$(document).ready(function(){
+    // Kör denna kod när sidan laddas
+    var ourUser = "test"
+    var ourPassword = "password"
+    
+    $(".helloForm").hide();
+    $(".forgotForm").hide();
+    // Klicka på logga in
+    $(".loginbutton").click(function(){
+        
+        if (ourUser == $(".userEmail").val() && ourPassword == $(".userPassword").val()) {
+                console.log("user mail is ok");
+                // Dölj inlogg Visa hello
+                $(".hejString").append(" hej " + $(".userEmail").val() );
+                $(".loginForm").hide();
+                $(".helloForm").show();
+            } else {
+                console.log("fel ifyllt")
+                // Dölj inlogg visa forgotPass
+                $(".loginForm").hide();
+                $(".forgotForm").show();
+            }
+    });
+
+});
