109e1fc09d4130e6eb2135eeab6233340a608c34
Andreeez <32762098+Andreeez@users.noreply.github.com>
2017-11-16 13:04:45 +0100

    Saving before making new test

--- script.js
+++ script.js
@@ -14,62 +14,14 @@ $(document).ready(function(){
     $(".welcome").show();
     // Klicka på logga in
 
-    $(".loginbutton").click(function(){
-        
-        /**if (sessionStorage.ourUser ) {
-            $(".welcome").show();
-
-         } else if {
-                $(".hejString").append(" hej " + $(".userEmail").val() );
-                $(".loginForm").hide();
-                $(".welcome").hide();
-                $(".helloForm").show();
-                $(".loginbutton").hide();
-                $(".logoutbutton").show();
-                $(".userEmail").hide();
-                $(".userPassword").hide();
-            }
-            else {
-                console.log("fel ifyllt")
-                // Dölj inlogg visa forgotPass
-                
-                $(".loginForm").hide();
-                $(".welcome").hide();
-                $(".forgotForm").show();
-            }
-
-        }    
-
-        //}
-
-
-
-
-        /**if (ourUser == $(".userEmail").val() && ourPassword == $(".userPassword").val()) {
-                console.log("user mail is ok");
-                // Dölj inlogg Visa hello
-                $(".hejString").append(" hej " + $(".userEmail").val() );
-                $(".loginForm").hide();
-                $(".welcome").hide();
-                $(".helloForm").show();
-                $(".loginbutton").hide();
-                $(".logoutbutton").show();
-                $(".userEmail").hide();
-                $(".userPassword").hide();
-            } else {
-                console.log("fel ifyllt")
-                // Dölj inlogg visa forgotPass
-                
-                $(".loginForm").hide();
-                $(".welcome").hide();
-                $(".forgotForm").show();
-            }*/
-
+           
             if (sessionStorage.ourUser !=null) {
+               
                 // Gör vad som ska göras om vi redan är inloggade.
                 } else {
                 // visa sidan för en ej inloggad men login funktion tex
-                               
+                $(".loginbutton").click(function(){
+                        
                   if (sessionStorage.ourUser == $(".userEmail").val() && sessionStorage.ourPassword == $(".userPassword").val()) {
                     console.log("user mail is ok");
                     // Dölj inlogg Visa hello
@@ -89,11 +41,9 @@ $(document).ready(function(){
                     $(".welcome").hide();
                     $(".forgotForm").show();
                 }
-                }
+            }); }
 
-
-
-    });
+    
 
 });
 
