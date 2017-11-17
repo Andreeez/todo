ea2f9de3e36dabec2cbbb05435d15543e0ba823c
Andreeez <32762098+Andreeez@users.noreply.github.com>
2017-11-17 10:20:26 +0100

    Updating login works now

--- script.js
+++ script.js
@@ -1,29 +1,67 @@
 console.log("hej");
 
 $(document).ready(function(){
-    // Kör denna kod när sidan laddas
-//var ourUser = "test"
-  //  var ourPassword = "password"
+    var ourUser = "test"; 
+    var ourPassword = "password";
     
-    sessionStorage.ourUser = "test"
-    sessionStorage.ourPassword = "password"
-
-    
-    $(".helloForm").hide();
+    $(".logoutbutton").hide();
     $(".forgotForm").hide();
-    $(".welcome").show();
-    // Klicka på logga in
+    
+    if(sessionStorage.userName !=null){
+        $(".welcome").hide();
+        $(".hello").text(sessionStorage.userName);
+        $(".loginbutton").hide();
+        $("input, label").hide();
+        $(".logoutbutton").show();
+        $(".forgotForm").hide();
+    } else {
+        $(".loginbutton").click(function(){
+            if (ourUser == $(".userEmail").val() && ourPassword == $(".userPassword").val()) {
+                sessionStorage.userName = "Anders Hagelkvist";
+                $(".welcome").hide();
+                $(".hello").text(sessionStorage.userName);
+                $(".loginbutton").hide();
+                $("input, label").hide();
+                $(".logoutbutton").show();
+                $(".forgotForm").hide();
 
-           
+            } else {
+                
+                $(".inloggform").show();
+                //$(".loginbutton").show();
+            }
+
+        });
+    }
+        $(".logoutbutton").click(function(){
+            sessionStorage.clear()
+            location.reload();
+        
+        });
+
+});
+
+
+   /** $(document).ready(function(){
+        var ourUser = "test"
+        var ourPassword = "password"
+
             if (sessionStorage.ourUser !=null) {
                
                 // Gör vad som ska göras om vi redan är inloggade.
                 } else {
+                    
+                        // Kör denna kod när sidan laddas
+
+                        
                 // visa sidan för en ej inloggad men login funktion tex
                 $(".loginbutton").click(function(){
-                        
-                  if (sessionStorage.ourUser == $(".userEmail").val() && sessionStorage.ourPassword == $(".userPassword").val()) {
+                    sessionStorage.ourUser = "test"
+                    sessionStorage.ourPassword = "password"
+                  if (ourUser == $(".userEmail").val() && ourPassword == $(".userPassword").val()) {
                     console.log("user mail is ok");
+                   
+
                     // Dölj inlogg Visa hello
                     $(".hejString").append(" hej " + $(".userEmail").val() );
                     $(".loginForm").hide();
@@ -41,12 +79,7 @@ $(document).ready(function(){
                     $(".welcome").hide();
                     $(".forgotForm").show();
                 }
-            }); }
+            }); 
 
-    
-
-});
-
-
-
+}); */
 
